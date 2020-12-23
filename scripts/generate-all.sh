445a55fefee5459a58930ccf239b40029d07c360
Andrew Childs <andrew.childs@bibo.com.ph>
2020-12-23 17:12:11 +0900

    Include aliases from symlinks in /usr/lib

--- scripts/generate-all.sh
+++ scripts/generate-all.sh
@@ -37,4 +37,6 @@ mkdir -p "$out"
 
 @out@/libexec/frameworks-tbd.sh -s "$sysroot" -o "$out"
 
+@out@/libexec/add-aliases.sh -s "$sysroot" -o "$out"
+
 @out@/libexec/link-frameworks.rb "$out/System/Library/**/*.tbd"
